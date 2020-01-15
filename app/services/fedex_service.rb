2ca4a4d921c6d77d759e7a4d9b52667b7b91e063
Abimael Martell <abimex@gmail.com>
2020-01-15 11:05:12 -0800

    Use dotenv for credentials

--- app/services/fedex_service.rb
+++ app/services/fedex_service.rb
@@ -6,23 +6,30 @@ class FedexService
   end
 
   def track_package(tracking_number)
-    begin
-      fedex_api.track(tracking_number: tracking_number)
-    rescue Fedex::RateError => error
-      @retries ||= 0
+    Rails.logger.debug "Fetching Fedex Data for #{tracking_number}"
+    fedex_api.track(tracking_number: tracking_number)
+  rescue Fedex::RateError => error
+    @retries ||= 0
 
-      if @retries < MAX_RETRIES
-        @retries += 1
-        retry
-      else
-        raise error
-      end
+    Rails.logger.debug "Error when fetching Fedex Data for #{tracking_number}"
+
+    if @retries < MAX_RETRIES
+      @retries += 1
+      retry
+    else
+      raise error
     end
   end
 
-private
+  private
 
   def credentials
-    Rails.application.credentials.fedex
+    {
+      key: ENV["FEDEX_KEY"],
+      password: ENV["FEDEX_PASSWORD"],
+      account_number: ENV["FEDEX_ACCOUNT_NUMBER"],
+      meter: ENV["FEDEX_METER"],
+      mode: ENV["FEDEX_MODE"]
+    }
   end
 end
